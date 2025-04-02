1ff51af09d2f146809b1f4b1e1d370c13f5cf8d3
pitbullturco <164184491+pitbullturco@users.noreply.github.com>
2025-04-02 01:35:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Bot WhatsApp rodando!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
