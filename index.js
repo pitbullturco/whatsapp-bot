88949188e3e4aa9364318fc0fe4def11a7b2c14b
pitbullturco <164184491+pitbullturco@users.noreply.github.com>
2025-04-02 14:34:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
